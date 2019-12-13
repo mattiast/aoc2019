ccf861081a45629ad8a0d6aea5402a85e967dae9
Matti Astrand <mattiastr@gmail.com>
2019-12-13 01:59:29 +0000

    Day 12 part 2 done

--- src/day12.rs
+++ src/day12.rs
@@ -1,85 +1,78 @@
-use derive_more::{Add, AddAssign};
 use std::cmp::Ordering;
 
-#[derive(Add, AddAssign, Clone, Copy)]
-struct Vec3<T>(T, T, T);
-
-struct MoonPosition {
-    pos: Vec3<i64>,
-    vel: Vec3<i64>,
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct Position {
+    pos: i64,
+    vel: i64,
 }
 
-impl MoonPosition {
+impl Position {
     fn move_pos(&mut self) {
         self.pos += self.vel;
     }
 
-    fn energy(&self) -> i64 {
-        let Vec3(x,y,z) = self.pos;
-        let potential = x.abs() + y.abs() + z.abs();
-        let Vec3(vx,vy,vz) = self.vel;
-        let kinetic = vx.abs() + vy.abs() + vz.abs();
-
-        potential * kinetic
-    }
-
-}
-
-fn gravity(m1: &MoonPosition, m2: &MoonPosition) -> Vec3<i64> {
-    let Vec3(x1, y1, z1) = m1.pos;
-    let Vec3(x2, y2, z2) = m2.pos;
-
-    let ax = match x1.cmp(&x2) {
-        Ordering::Equal => 0,
-        Ordering::Less => 1,
-        Ordering::Greater => -1,
-    };
-    let ay = match y1.cmp(&y2) {
-        Ordering::Equal => 0,
-        Ordering::Less => 1,
-        Ordering::Greater => -1,
-    };
-    let az = match z1.cmp(&z2) {
-        Ordering::Equal => 0,
-        Ordering::Less => 1,
-        Ordering::Greater => -1,
-    };
-    Vec3(ax, ay, az)
-}
-
-fn apply_gravity(moons: &mut [MoonPosition]) {
-    let n = moons.len();
-    for i in 0..n {
-        for j in 0..n {
-            moons[i].vel += gravity(&moons[i], &moons[j]);
+    fn apply_gravity(moons: &mut [Position]) {
+        let n = moons.len();
+        for i in 0..n {
+            for j in 0..n {
+                moons[i].vel += match moons[i].pos.cmp(&moons[j].pos) {
+                    Ordering::Equal => 0,
+                    Ordering::Less => 1,
+                    Ordering::Greater => -1,
+                };
+            }
         }
     }
 }
 
-fn one_round(moons: &mut [MoonPosition]) {
-    apply_gravity(moons);
+fn one_round(moons: &mut [Position]) {
+    Position::apply_gravity(moons);
     for moon in moons {
         moon.move_pos();
     }
 }
 
-pub fn part1() {
-    let mut moons = vec![
-        MoonPosition {pos: Vec3(-8, -9, -7), vel: Vec3(0,0,0)},
-        MoonPosition {pos: Vec3(-5, 2, -1), vel: Vec3(0,0,0)},
-        MoonPosition {pos: Vec3(11, 8, -14), vel: Vec3(0,0,0)},
-        MoonPosition {pos: Vec3(1, -4, -11), vel: Vec3(0,0,0)},
-    ];
-    for _ in 0..1000 {
-        one_round(&mut moons);
+fn get_round_length(moons: &mut [Position]) -> usize {
+    let original: Vec<_> = moons.to_vec();
+    one_round(moons);
+    let mut n = 1;
+    loop {
+        let mats = moons.iter().zip(original.iter()).all(|(x, y)| x == y);
+        if mats {
+            return n;
+        }
+        one_round(moons);
+        n += 1;
     }
-    let total_energy: i64 = moons.iter().map(|m| m.energy()).sum();
-    println!("total energy {:?}", total_energy);
+}
+
+use num::integer::lcm;
+pub fn part2() {
+    let mut moons_x = vec![
+        Position { pos: -8, vel: 0 },
+        Position { pos: -5, vel: 0 },
+        Position { pos: 11, vel: 0 },
+        Position { pos: 1, vel: 0 },
+    ];
+    let mut moons_y = vec![
+        Position { pos: -9, vel: 0 },
+        Position { pos: 2, vel: 0 },
+        Position { pos: 8, vel: 0 },
+        Position { pos: -4, vel: 0 },
+    ];
+    let mut moons_z = vec![
+        Position { pos: -7, vel: 0 },
+        Position { pos: -1, vel: 0 },
+        Position { pos: -14, vel: 0 },
+        Position { pos: -11, vel: 0 },
+    ];
+    let x_round = get_round_length(&mut moons_x);
+    let y_round = get_round_length(&mut moons_y);
+    let z_round = get_round_length(&mut moons_z);
+    println!("{}", lcm(x_round, lcm(y_round, z_round)));
 }
 
 //  <x=-8, y=-9, z=-7>
 //  <x=-5, y=2, z=-1>
 //  <x=11, y=8, z=-14>
 //  <x=1, y=-4, z=-11>
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 fn main() -> io::Result<()> {
-    advent::day12::part1();
+    advent::day12::part2();
 
     Ok(())
 }
