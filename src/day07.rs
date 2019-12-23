c22bc46ee22bdd4ec476d032078c988b63bae8ec
Matti Astrand <mattiastr@gmail.com>
2019-12-23 15:29:20 +0000

    Simplified day 7 part 1

--- src/day07.rs
+++ src/day07.rs
@@ -12,56 +12,29 @@ pub fn read_input() -> io::Result<Vec<isize>> {
     Ok(numbers)
 }
 
-fn execute_until_termination(
-    program: &mut Vec<isize>,
-    inputs: Vec<isize>,
-) -> Result<isize, &'static str> {
-    let mut state = ProgramState::init(program.clone());
-    let mut outs: Vec<isize> = vec![];
-    let mut iter = inputs.into_iter();
-    let mut input = iter.next();
-    while !state.terminated {
-        let inst = state.parse_instruction()?;
-        if input.is_none() {
-            input = iter.next();
-        }
-        let output = state.execute_instruction(inst, &mut input)?;
-        if let Some(out) = output {
-            outs.push(out);
-        }
+fn get_5_stage(program: &ProgramState, phases: &[isize]) -> Result<isize, &'static str> {
+    let mut x = 0;
+    for phase in phases.iter() {
+        let mut program = program.clone();
+        let (outs, k) = program.run_with_input(&[*phase, x]).unwrap();
+        assert!(k == 2);
+        assert!(outs.len() == 1);
+        x = outs[0];
     }
-    assert!(iter.next() == None);
-    assert!(outs.len() == 1);
-    Ok(outs[0])
-}
-
-fn get_output(program: &[isize], phase: isize, input: isize) -> Result<isize, &'static str> {
-    let mut prog_copy = Vec::from(program);
-    execute_until_termination(&mut prog_copy, vec![phase, input])
-}
-
-fn get_5_stage(program: &[isize], phases: &[isize]) -> Result<isize, &'static str> {
-    let x1 = get_output(&program, phases[0], 0)?;
-    let x2 = get_output(&program, phases[1], x1)?;
-    let x3 = get_output(&program, phases[2], x2)?;
-    let x4 = get_output(&program, phases[3], x3)?;
-    let x5 = get_output(&program, phases[4], x4)?;
-    Ok(x5)
+    Ok(x)
 }
 
 use permutator::Permutation;
-pub fn part1() -> io::Result<()> {
-    let prog = read_input()?;
+pub fn part1() -> io::Result<isize> {
+    let ps = ProgramState::init_from_file("data/input07.txt")?;
 
     let mut data = vec![0, 1, 2, 3, 4];
     let max_output = data
         .permutation()
-        .map(|x| get_5_stage(&prog, &x).unwrap())
+        .map(|x| get_5_stage(&ps, &x).unwrap())
         .max();
 
-    println!("{:?}", max_output);
-
-    Ok(())
+    Ok(max_output.unwrap())
 }
 
 fn get_5_stage_feedback(program: &[isize], phases: &[isize]) -> Result<isize, &'static str> {
@@ -103,7 +76,7 @@ fn get_5_stage_feedback(program: &[isize], phases: &[isize]) -> Result<isize, &'
     Ok(inputs[0][0])
 }
 
-pub fn part2() -> io::Result<()> {
+pub fn part2() -> io::Result<isize> {
     let prog = read_input()?;
 
     let mut data = vec![5, 6, 7, 8, 9];
@@ -112,18 +85,16 @@ pub fn part2() -> io::Result<()> {
         .map(|x| get_5_stage_feedback(&prog, &x).unwrap())
         .max();
 
-    println!("{:?}", max_output);
-
-    Ok(())
+    Ok(max_output.unwrap())
 }
 
 #[test]
 fn test_5_stage() {
-    let prog = &[
+    let prog = vec![
         3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33,
         31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0,
     ];
     let phases = &[1, 0, 4, 3, 2];
 
-    assert_eq!(get_5_stage(prog, phases), Ok(65210));
+    assert_eq!(get_5_stage(&ProgramState::init(prog), phases), Ok(65210));
 }
--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,18 @@ fn _main() -> io::Result<()> {
 }
 
 fn main() -> io::Result<()> {
-    let x = advent::day02::part1()?;
-    println!("Day 2 part 1: {}", x);
+    println!("Day 2 part 1: {:?}", advent::day02::part1()?);
+    println!("Day 2 part 2: {:?}", advent::day02::part2()?);
 
-    let x = advent::day02::part2()?;
-    println!("Day 2 part 2: {:?}", x);
+    println!("Day 5 part 1: {:?}", advent::day05::part1()?);
+    println!("Day 5 part 2: {:?}", advent::day05::part2()?);
 
-    let r = advent::day05::part1()?;
-    println!("Day 5 part 1: {:?}", r);
-    let r = advent::day05::part2()?;
-    println!("Day 5 part 2: {:?}", r);
+    println!("Day 7 part 1: {:?}", advent::day07::part1()?);
+    println!("Day 7 part 2: {:?}", advent::day07::part2()?);
 
-    advent::day07::part1()?;
-    advent::day07::part2()?;
+    println!("Day 9 part 1: {:?}", advent::day09::part1()?);
+    println!("Day 9 part 2: {:?}", advent::day09::part2()?);
 
-    let r = advent::day09::part1()?;
-    println!("Day 9 part 1: {:?}", r);
-    let r = advent::day09::part2()?;
-    println!("Day 9 part 2: {:?}", r);
-
-    advent::day11::part1()?;
+    // advent::day11::part1()?;
     Ok(())
 }
