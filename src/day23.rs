101a03ed25fb8a1e88e83fb195874c46d8b3b057
Matti Astrand <mattiastr@gmail.com>
2019-12-23 06:05:20 +0000

    Day 23 done

--- src/day23.rs
+++ src/day23.rs
@@ -39,3 +39,57 @@ pub fn part1() -> io::Result<()> {
 
     Ok(())
 }
+
+pub fn part2() -> io::Result<()> {
+    let ps = ProgramState::init_from_file("data/input23.txt")?;
+
+    let mut computers: Vec<_> = (0..50).map(|_| ps.clone()).collect();
+    let mut mailboxes: Vec<Vec<(isize, isize)>> = (0..50).map(|_| Vec::new()).collect();
+
+    let mut nat: Option<(isize, isize)> = None;
+
+    for (i, computer) in computers.iter_mut().enumerate() {
+        let inp: &[isize] = &[i as isize];
+        let (out, _) = computer.run_with_input(inp).unwrap();
+        assert!(out.len() % 3 == 0);
+        for j in 0..out.len() / 3 {
+            let (addr, x, y) = (out[3 * j], out[3 * j + 1], out[3 * j + 2]);
+            mailboxes[addr as usize].push((x, y));
+        }
+    }
+    let mut last_nat_msg: Option<isize> = None;
+    loop {
+        let mut idle = true;
+        for i in 0..50 {
+            let inp: Vec<isize> = if mailboxes[i].is_empty() {
+                vec![-1]
+            } else {
+                idle = false;
+                let (x, y) = mailboxes[i].remove(0);
+                vec![x, y]
+            };
+            let (out, _) = computers[i].run_with_input(&inp).unwrap();
+            assert!(out.len() % 3 == 0);
+            for j in 0..out.len() / 3 {
+                idle = false;
+                let (addr, x, y) = (out[3 * j], out[3 * j + 1], out[3 * j + 2]);
+                if addr == 255 {
+                    nat = Some((x, y));
+                } else {
+                    mailboxes[addr as usize].push((x, y));
+                }
+            }
+        }
+        if idle {
+            println!("nat says {:?}", nat);
+            let nat_msg = nat.unwrap();
+            if last_nat_msg == Some(nat_msg.1) {
+                break;
+            }
+            last_nat_msg = Some(nat_msg.1);
+            mailboxes[0].push(nat_msg);
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 fn main() -> io::Result<()> {
-    advent::day23::part1()?;
+    advent::day23::part2()?;
 
     Ok(())
 }
