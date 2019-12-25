9561ba2e110c46aac38b082b5e0a8ab19e5cae7e
Matti Astrand <mattiastr@gmail.com>
2019-12-25 18:16:14 +0000

    Day 25 cleanup

--- src/day25.rs
+++ src/day25.rs
@@ -14,17 +14,6 @@ pub fn part1() -> io::Result<()> {
     let (out, _) = ps.run_with_input(&[]).unwrap();
     addstr(&get_string(out));
     while !ps.terminated {
-        /*
-        let c = getch();
-        println!("{}", c);
-        let (line, dir) = match c {
-            104 => ("west", (-1, 0)),
-            106 => ("south", (0, -1)),
-            107 => ("north", (0, 1)),
-            108 => ("east", (1, 0)),
-            _ => continue,
-        };
-        */
         let mut line = String::new();
         getstr(&mut line);
         let mut inp: Vec<_> = line.as_bytes().iter().map(|c| *c as isize).collect();
@@ -33,8 +22,6 @@ pub fn part1() -> io::Result<()> {
         clear();
         let s = get_string(out);
         addstr(&s);
-        // mv(20, 0);
-        // addstr(&state.render());
         if ps.terminated {
             endwin();
             println!("{}", s);
