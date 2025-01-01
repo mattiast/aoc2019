89b3482847adeac0794dad2c10ace8561b02d88e
Matti Astrand <mattiastr@gmail.com>
2025-01-01 13:21:13 -0500

    Output

--- src/day21.rs
+++ src/day21.rs
@@ -3,23 +3,22 @@ use crate::my_error::MyResult;
 
 use std::fs::File;
 use std::io::Read;
-pub fn part1() -> MyResult<()> {
+pub fn part1(path: &str) -> MyResult<()> {
     let mut ps = ProgramState::init_from_file("data/input21.txt")?;
-
     let (prompt, _) = ps.run_with_input(&[])?;
 
     let s = String::from_utf8(prompt.iter().map(|x| *x as u8).collect()).unwrap();
-    print!("{}", s);
+    print!("Prompt: {}", s);
 
     let input: Vec<_> = {
-        let mut file = File::open("day21code2.txt")?;
+        let mut file = File::open(path)?;
 
         let mut data = Vec::new();
         file.read_to_end(&mut data)?;
         data.into_iter().map(|x| x as isize).collect()
     };
     let (stuff, n) = ps.run_with_input(&input)?;
-    println!("{} == {}", n, input.len());
+    println!("Check that all input was read: {} == {}", n, input.len());
     let s = String::from_utf8(stuff.iter().map(|x| *x as u8).collect()).unwrap();
     print!("{}", s);
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use advent::my_error::MyResult;
+use advent::{day21, my_error::MyResult};
+use clap::{arg, command};
 
 fn _main() -> MyResult<()> {
     advent::day19::part2()?;
@@ -6,22 +7,11 @@ fn _main() -> MyResult<()> {
 }
 
 fn main() -> MyResult<()> {
-    println!("Day 2 part 1: {:?}", advent::day02::part1()?);
-    println!("Day 2 part 2: {:?}", advent::day02::part2()?);
+    tracing_subscriber::fmt::init();
 
-    println!("Day 5 part 1: {:?}", advent::day05::part1()?);
-    println!("Day 5 part 2: {:?}", advent::day05::part2()?);
+    let matches = command!().arg(arg!([file] "file")).get_matches();
+    let path = matches.get_one::<String>("file").unwrap();
 
-    println!("Day 7 part 1: {:?}", advent::day07::part1()?);
-    println!("Day 7 part 2: {:?}", advent::day07::part2()?);
-
-    println!("Day 9 part 1: {:?}", advent::day09::part1()?);
-    println!("Day 9 part 2: {:?}", advent::day09::part2()?);
-
-    println!("Day 11 part 1: {:?}", advent::day11::part1()?);
-    advent::day11::part2()?;
-
-    println!("Day 19 part 1: {:?}", advent::day19::part1()?);
-
+    day21::part1(path)?;
     Ok(())
 }
